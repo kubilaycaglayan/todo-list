9731a0bf4b7cc3ebda4b40003531fabe49e5b431
kubilaycaglayan <kblycaglayan@gmail.com>
2020-07-06 15:26:40 +0300

    Add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,16 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
 };
