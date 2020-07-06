86545594dd02e3b1f46f034725198b54c44e5d3a
kubilaycaglayan <kblycaglayan@gmail.com>
2020-07-06 15:40:41 +0300

    Create factory function for todoItem

---
+++ src/models/toDoItem.js
@@ -0,0 +1,12 @@
+const createToDoItem = function createToDoItem(title, description, dueDate, priority, notes, checklist = false) {
+  return {
+    title,
+    description,
+    dueDate,
+    priority,
+    notes,
+    checklist,
+  };
+};
+
+export { createToDoItem as default };
