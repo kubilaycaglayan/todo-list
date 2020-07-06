38739cb1e648a4bcec546bf1277a9fd2adf904a2
javitocor <56809963+javitocor@users.noreply.github.com>
2020-07-06 15:45:26 +0200

    Add eventlistener to create new projects

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
+import { storage } from './models/storage.js'
 import './style.css';
 import firstProject from './models/defaultProject'
 
 console.log('Success');
 console.log(firstProject);
+
+const nameProject = document.getElementById('nameProject').value;
+const submitProject = document.getElementById('submitProject');
+
+function createProject() {
+    const nameProject = document.getElementById('nameProject').value;
+    const localStorage = storage();
+    let newProject = createProject(nameProject);
+    localStorage.addProject(newProject);
+}
+    
+submitProject.addEventListener('click', addProject());
---
+++ src/models/DOM.js
@@ -0,0 +1,4 @@
+const Dom = function Dom() {
+
+    
+}
