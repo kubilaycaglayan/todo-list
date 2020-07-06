5e33180f0096d5d0567622b4c14a7a605c7c271c
javitocor <56809963+javitocor@users.noreply.github.com>
2020-07-06 15:26:45 +0200

    Add storage module

---
+++ src/models/storage.js
@@ -0,0 +1,53 @@
+const storage = function storage() {
+
+  function getStorage() {
+    let toDoList;
+    if (window.localStorage.getItem('todolist') === null) {
+      toDoList = [];
+      window.localStorage.setItem('todolist', JSON.stringify(toDoList));
+    } else {
+      toDoList = JSON.parse(window.localStorage.getItem('todolist'));
+    }
+    return toDoList;
+  }
+
+  function addProject(newProject) {
+    let storage = getStorage();
+    storage.push(newProject);
+    window.localStorage.setItem('todolist', JSON.stringify(storage));
+  }
+
+  function deleteProject(projectId) {
+    const confirmation = window.confirm;
+    if(confirmation){
+      let storage = getStorage();
+      storage.splice(projectId,1);
+      window.localStorage.setItem('todolist', JSON.stringify(storage));
+    }    
+  }
+
+  function addTodo(projectId, item) {
+    let storage = getStorage();
+    storage[projectId].pocket.push(item);
+    window.localStorage.setItem('todolist', JSON.stringify(storage));
+  }
+
+  function deleteTodo(projectId, itemId) {
+    const confirmation = window.confirm;
+    if(confirmation){
+      let storage = getStorage();
+      storage[projectId].pocket.splice(itemId,1);
+      window.localStorage.setItem('todolist', JSON.stringify(storage));
+    }    
+  }
+
+  return {
+    getStorage,
+    addProject,
+    deleteProject,
+    addTodo,
+    deleteTodo,
+  }
+};
+
+export { storage };
